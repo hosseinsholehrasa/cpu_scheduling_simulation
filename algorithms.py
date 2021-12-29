16dec0a387e5b00d70d465b5e5b3739d9f96d071
hosseinsho <amiramiry125@gmail.com>
2021-12-29 13:42:59 +0330

    init commit

---
+++ algorithms.py
@@ -0,0 +1,22 @@
+def FIFO(processes: list):
+    pass
+
+
+def preemptive_SJF(processes: list):
+    pass
+
+
+def non_preemptive_SJF(processes: list):
+    pass
+
+
+def RR(processes: list):
+    pass
+
+
+def priority_preemption(processes: list):
+    pass
+
+
+def non_priority_preemption(processes: list):
+    pass
---
+++ process.py
@@ -0,0 +1,8 @@
+
+
+class Process:
+    def __init__(self, pid, arrival_time, priority):
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.priority = priority
+
