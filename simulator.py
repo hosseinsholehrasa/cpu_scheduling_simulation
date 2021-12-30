2337fd271ecf4f73c0d21815184c88d7b12847ee
hosseinsho <amiramiry125@gmail.com>
2021-12-31 01:12:42 +0330

    feat: add simulator class and it's attributes

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import algorithms
 
 
 def get_cpu_time_unit():
@@ -16,4 +17,36 @@ def get_cpu_time_unit():
     ended_at = datetime.now()
     return ended_at - started_at
 
+
+class Simulator:
+
+    def __init__(self, algorithm: str):
+        self.algorithm = algorithm
+        self.processes = []
+        self.run_time = 0
+        self.cpu_total_time = 0
+        self.cpu_utilization = 0
+        self.throughput = 0
+        self.average_waiting_time = 0.0
+        self.average_turnaround_time = 0.0
+        self.average_response_time = 0.0
+
+    def read_processes_data(self, data_dir: str, dataframe=None) -> list:
+        """
+        read data from csv file or pandas dataframe
+        :return a list of process object:
+        """
+        pass
+
+    def get_algorithm(self):
+        """
+        find algorithm class based on algorithm folder
+        :return a class
+        """
+        try:
+            return getattr(algorithms, self.algorithm)
+        except Exception as e:
+            print(e)
+            print("try again!")
+            exit()
 
