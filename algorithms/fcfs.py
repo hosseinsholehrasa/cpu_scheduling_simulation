66820da564c664eb3e941bd2ec26e9e68ae6054d
hosseinsho <amiramiry125@gmail.com>
2021-12-31 21:17:47 +0330

    refactor: add timeline and cpu idle time to attributes

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -9,6 +9,8 @@ class FCFS(object):
         """
         self.processes = processes
         self.processes.sort(key=lambda process: process.arrival_time)
+        self.timeline = 0.0
+        self.cpu_idle_time = 0.0
         self.executed_processes = []
         self.suspended_processes = []
 
@@ -21,27 +23,26 @@ class FCFS(object):
             "cpu_idle_time": time that cpu was idle,
         }
         """
-        timeline = 0.0
-        cpu_idle_time = 0.0
+
         for process in self.processes:
 
             # cpu waiting time
-            if process.arrival_time > timeline:
-                cpu_idle_time += process.arrival_time - timeline
+            if process.arrival_time > self.timeline:
+                self.cpu_idle_time += process.arrival_time - self.timeline
                 # update time
                 timeline = process.arrival_time
 
             # Run the process
-            process.start_time = timeline
+            process.start_time = self.timeline
 
             # waiting, turnaround, response time
-            process.waiting_time = timeline - process.arrival_time
-            process.response_time = timeline - process.arrival_time
+            process.waiting_time = self.timeline - process.arrival_time
+            process.response_time = self.timeline - process.arrival_time
             process.turnaround_time = process.response_time + process.burst_time
 
-            timeline += process.burst_time
+            self.timeline += process.burst_time
             process.remaining_time -= process.burst_time
-            process.end_time = timeline
+            process.end_time = self.timeline
 
             process.state = State.EXECUTED
             print(process)
@@ -50,6 +51,6 @@ class FCFS(object):
 
         return {
             "executed_processes": self.executed_processes,
-            "cpu_total_time": timeline,
-            "cpu_idle_time": cpu_idle_time,
+            "cpu_total_time": self.timeline,
+            "cpu_idle_time": self.cpu_idle_time,
         }
