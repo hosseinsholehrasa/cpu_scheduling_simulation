5bb7b586d1614c5af1163c6c6a5a04ff09012387
hosseinsho <amiramiry125@gmail.com>
2022-01-01 18:11:29 +0330

    feat: complete preemptive priority run method

--- algorithms/preemptive_priority.py
+++ algorithms/preemptive_priority.py
@@ -45,3 +45,77 @@ class PreemptivePriority(object):
                 # All processes have done
                 if not (self.running_process or self.processes or self.ready_queue):
                     break
+
+            arrived_processes = []
+            # processes that their arrival time are equal to this timeline goes to ready queue
+            for process in self.processes:
+                if process.arrival_time == self.timeline:
+                    arrived_processes.append(process)
+                # for finishing sooner
+                elif process.arrival_time > self.timeline:
+                    break
+
+            # for deleting new arrived processes from self.processes list
+            for process in arrived_processes:
+                if process in self.processes:
+                    self.processes.remove(process)
+
+            arrived_processes.sort(key=lambda p: p.priority)
+            if arrived_processes:
+                if self.running_process is None:
+                    self.running_process = arrived_processes.pop(0)
+                    self.running_process.start_time = self.timeline
+
+                # new arrived processes have higher priority
+                elif self.running_process.priority > arrived_processes[0].priority:
+                    self.ready_queue.append(self.running_process)
+                    self.running_process = arrived_processes.pop(0)
+                    self.running_process.start_time = self.timeline
+
+                # add arrived processes to ready queue
+                self.ready_queue += arrived_processes
+
+                # free this variable memory
+                arrived_processes.clear()
+
+            # If no process is running, then pick the process from ready queue, maybe a process has ran before
+            if self.running_process is None and self.ready_queue:
+                self.ready_queue.sort(key=lambda x: x.priority)
+                self.running_process = self.ready_queue.pop(0)
+                self.running_process.start_time = self.running_process.start_time or self.timeline
+
+            # run process until some important things happen
+            next_important_time = self.get_next_important_time()
+            if self.running_process:
+                self.running_process.remaining_time -= (next_important_time - self.timeline)
+            else:
+                self.cpu_idle_time += (next_important_time - self.timeline)
+            self.timeline = next_important_time
+
+        return {
+            "executed_processes": self.executed_processes,
+            "cpu_total_time": self.timeline,
+            "cpu_idle_time": self.cpu_idle_time,
+        }
+
+    def get_next_important_time(self):
+        """
+        Find next important things which happen in the timeline
+        :return: Int
+        """
+        # we have deleted process from list when we decided about it to be an ready queue or running process
+        future_processes = self.processes
+        self.ready_queue.sort(key=lambda p: p.priority)
+        if future_processes:
+            # we have some conditions here. we have to compare ready queue and next process with their arrival time and
+            # priority so just update time with 1
+            if not self.running_process:
+                return self.timeline + 1
+            # if we have running process when we have future processes,
+            # the important time is next arrival time or finishing the process
+            return min((self.running_process.remaining_time or 1) + self.timeline, future_processes[0].arrival_time)
+        else:
+            if not self.running_process:
+                return self.timeline + 1
+            # we don't have new process and so we should wait until process finish it's work
+            return self.running_process.remaining_time + self.timeline
